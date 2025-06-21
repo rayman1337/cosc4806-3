e6f1913893d89ec1c99325b0bc5cbba908344321
RM <134272733+rayman1337@users.noreply.github.com>
2025-06-21 02:57:08 +0000

    fixed error messages not passing to view

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -2,16 +2,23 @@
 
 class Login extends Controller {
 
-    public function index() {		
-	    $this->view('login/index');
-    }
-    
-    public function verify(){
-			$username = $_REQUEST['username'];
-			$password = $_REQUEST['password'];
-		
-			$user = $this->model('User');
-			$user->authenticate($username, $password); 
-    }
+		public function index() {
+				$error = $_SESSION['error'] ?? null;
+				unset($_SESSION['error']); 
 
+				$this->view('login/index', ['error' => $error]);
+		}
+
+		public function verify() {
+				if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+						$username = trim($_POST['username']);
+						$password = trim($_POST['password']);
+
+						$userModel = $this->model('User');
+						$userModel->authenticate($username, $password);
+				} else {
+						header('Location: /login');
+						exit;
+				}
+		}
 }
