98d045478ce3adbaeee9f4d72a303383745c2118
RM <134272733+rayman1337@users.noreply.github.com>
2025-06-21 02:32:15 +0000

    modified config file to match database structure

--- app/core/config.php
+++ app/core/config.php
@@ -14,8 +14,8 @@ define('LOGS', ROOT . DS . 'logs');
 define('FILES', ROOT . DS. 'files');
 
 // ---------------------  NEW DATABASE TABLE -------------------------
-define('DB_HOST',         'xxw.h.filess.io');
-define('DB_USER',         '4806_sentenceby'); 
-define('DB_PASS',         $_ENV['DB_PASS']);
-define('DB_DATABASE',     '4806_sentenceby');
-define('DB_PORT',         '3305');
+define('DB_HOST', '1yjbj.h.filess.io');
+define('DB_USER', '4806_milkpartly');
+define('DB_PASS', $_ENV['DB_PASS']);
+define('DB_DATABASE', '4806_milkpartly');
+define('DB_PORT', '3305');
